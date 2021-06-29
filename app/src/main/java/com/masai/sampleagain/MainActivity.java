b6a981fdcba71a1f874a7c73027aa52fe71c301f
Sai Krishna <crishnakorukanti@gmail.com>
2021-06-29 20:03:20 +0530

    Update in code

--- app/src/main/java/com/masai/sampleagain/MainActivity.java
+++ app/src/main/java/com/masai/sampleagain/MainActivity.java
@@ -3,12 +3,15 @@ package com.masai.sampleagain;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
     }
 }
